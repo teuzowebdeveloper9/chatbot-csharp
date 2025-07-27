aabd1374d87a070d593100dae104bda661d6744f
teuzowebdeveloper9 <mateussoftwaredeveloper@gmail.com>
2025-07-27 02:17:55 -0300

    feat : initialized frontend and create JSON from opinions

--- frontend/chatbot-ai/next.config.ts
+++ frontend/chatbot-ai/next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images : {
+  domains : [
+  "fly.metroimg.com",
+  "s2-epocanegocios.glbimg.com",
+  "i.s3.glbimg.com",
+  "all-images.ai",
+  "learn.microsoft.com",
+  "img.freepik.com",
+  "plataformamix.com.br",
+  "pbs.twimg.com",
+  "evertectrends.com",
+  "houseofmodels.com.br"
+],
+ },
 };
 
 export default nextConfig;
